bc5d11fa923c748a6d2754085c6d21503672f267
yoloabdo <dhooom.da@gmail.com>
2018-05-09 09:37:00 +0200

    Initial Commit

---
+++ SwiftCairo-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCairo-App
+//
+//  Created by abdelrahman mohamed on 5/9/18.
+//  Copyright © 2018 abdelrahman mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
