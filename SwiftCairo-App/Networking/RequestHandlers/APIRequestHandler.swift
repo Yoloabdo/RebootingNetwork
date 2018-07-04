26b814e51aaca0a8a0b017a72fca28a07f8e5275
yoloabdo <dhooom.da@gmail.com>
2018-07-04 06:26:18 +0200

    adding upload multipart

--- SwiftCairo-App/Networking/RequestHandlers/APIRequestHandler.swift
+++ SwiftCairo-App/Networking/RequestHandlers/APIRequestHandler.swift
@@ -18,25 +18,48 @@ typealias CallResponse<T> = ((ServerResponse<T>) -> Void)?
 /// API protocol, The alamofire wrapper
 protocol APIRequestHandler: HandleAlamoResponse {
     
-    /// Calling network layer via (Alamofire), this implementation can be replaced anytime in one place which is the protocol itsel, applied everywhere.
-    ///
-    /// - Parameters:
-    ///   - decoder: Codable confirmed class/struct, Model.type.
-    ///   - completion: Results of the request, general errors cases handled.
-    /// - Returns: Void.
-    func send<T: CodableInit>(_ decoder: T.Type, completion: CallResponse<T>)
+    
 }
 
-extension APIRequestHandler where Self: URLRequestConvertible {
+extension APIRequestHandler where Self: URLRequestBuilder {
 
-    func send<T: CodableInit>(_ decoder: T.Type, completion: CallResponse<T>) {
-        request(self).validate().responseData {(response) in
-            self.handleResponse(response, completion: completion)
+    func send<T: CodableInit>(_ decoder: T.Type, data: UploadData? = nil, progress: ((Progress) -> Void)? = nil, completion: CallResponse<T>) {
+        if let data = data {
+            uploadToServerWith(decoder, data: data, request: self, parameters: self.parameters, progress: progress, completion: completion)
+        }else{
+            request(self).validate().responseData {(response) in
+                self.handleResponse(response, completion: completion)
+            }
         }
     }
 }
 
 
+extension APIRequestHandler {
+    
+    private func uploadToServerWith<T: CodableInit>(_ decoder: T.Type, data: UploadData, request: URLRequestConvertible, parameters: Parameters?, progress: ((Progress) -> Void)?, completion: CallResponse<T>) {
+        
+        upload(multipartFormData: { (mul) in
+            mul.append(data.data, withName: data.name, fileName: data.fileName, mimeType: data.mimeType)
+            guard let parameters = parameters else { return }
+            for (key, value) in parameters {
+                mul.append("\(value)".data(using: String.Encoding.utf8)!, withName: key as String)
+            }
+        }, with: request) { (response) in
+            switch response {
+            case .success(let requestUp, _, _):
+                requestUp.responseData(completionHandler: { (results) in
+                    self.handleResponse(results, completion: completion)
+                }).responseString(completionHandler: { (string) in
+                    print(string.result.value)
+                })
+                
+            case .failure(let error):
+                completion?(ServerResponse<T>.failure(error as? LocalizedError))
+            }
+        }
+    }
+}
 
 
 
---
+++ SwiftCairo-App/Networking/Requests/UploadData.swift
@@ -0,0 +1,14 @@
+//
+//  DataFile.swift
+//  SwiftCairo-App
+//
+//  Created by abdelrahman mohamed on 7/4/18.
+//  Copyright © 2018 abdelrahman mohamed. All rights reserved.
+//
+
+import Foundation
+
+struct UploadData {
+    var data: Data
+    var fileName, mimeType, name: String
+}
